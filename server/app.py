d7dc9e14650863133029c0870e28987a825200b7
Akash NP <akashnp1998@gmail.com>
2023-09-23 07:26:26 +0400

    chore: cors allows origin from vite dev server

--- server/app.py
+++ server/app.py
@@ -1,12 +1,19 @@
 from decouple import config
 from fastapi import FastAPI
 from langchain.llms import OpenAI
-from langchain.llms.fake import FakeListLLM
+from fastapi.middleware.cors import CORSMiddleware
 
 from ai.prompts import modify_element_prompt
 from ai.llms import OpenAI
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 llm = OpenAI(openai_api_key=config("OPENAI_API_KEY"))
 
 @app.get("/")
