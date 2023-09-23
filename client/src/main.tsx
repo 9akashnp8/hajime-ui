dedcf22bc4828f792e473aeec8ffb55b6ef7afc7
Akash NP <akashnp1998@gmail.com>
2023-09-23 18:15:18 +0400

    feat: install redux and configure provider to make state available

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,9 @@ import {
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
+import { store } from './features/core/store.ts'
 import Root from './features/core/routes/root.tsx'
 import Home from './features/core/routes/home.tsx'
 import './index.css'
@@ -29,7 +31,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
 
