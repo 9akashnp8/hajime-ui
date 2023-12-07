859bc8c8ee22508f916a6f79e8538f5eead9a150
Akash NP <akashnp1998@gmail.com>
2023-12-07 22:37:22 +0400

    chore: update frontend to handle new api changes

--- client/src/features/canvas/routes/index.tsx
+++ client/src/features/canvas/routes/index.tsx
@@ -1,24 +1,22 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useSelector } from 'react-redux'
 
 import { RootState } from "../../core/store"
-import { useForceUpdate } from "../../../utils/hooks";
 
 export default function Canvas() {
-    const forceUpdate = useForceUpdate();
-    const ref = useRef<HTMLDivElement>(null);
     const generatedElement = useSelector((state: RootState) => state.generatedElement);
 
     useEffect(() => {
-        const template = document.createElement('template')
-        template.innerHTML = generatedElement.trim()
-        ref.current?.replaceWith(template.content.firstChild!)
-        forceUpdate() // rerender to get styles from (how would you do after building?)
-    }, [generatedElement])
+        const preview = document.createElement('iframe')
+        preview.setAttribute('srcDoc', generatedElement)
+        const previewSection = document.getElementById('previewSection')!;
+        previewSection.appendChild(preview);
+    }, [])
 
     return (
-        <section className="bg-slate-500 grid place-items-center h-[calc(100vh-100px)] mt-5 rounded">
-            <div id="canvasElement" ref={ref}></div>
-        </section>
+        <section
+            id="previewSection"
+            className="bg-slate-100 grid place-items-center h-[calc(100vh-100px)] mt-5 rounded"
+        ></section>
     )
 }
--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000',
-    withCredentials: true,
+    baseURL: 'http://localhost:3000',
 })
