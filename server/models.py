1e566a823abb011efb8f3647b5111fe1f34a8e76
Akash NP <akashnp1998@gmail.com>
2023-12-07 22:26:18 +0400

    chore!: refactor backend into node server to utilize postcss
    as of now, no solution to implement postcss in python. postcss
    will allow us to generate relevant css for given html with
    tailwind utilily classes

--- server/models.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class GenerateElementPayload(BaseModel):
-    prompt: str
---
+++ server/src/utils/constants.ts
@@ -0,0 +1,2 @@
+
+export const mockHtml = '<div class="bg-white shadow-md rounded-lg overflow-hidden">\n  <div class="p-4">\n    <div class="text-lg font-bold">Informative Card</div>\n    <div class="text-gray-700 text-base">\n      Here is the content of the card\n    </div>\n  </div>\n</div>';
