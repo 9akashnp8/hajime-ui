4c7c625d61afa214a2e1bec5c87c8c924fea0f4c
Akash NP <akashnp1998@gmail.com>
2023-12-09 09:54:27 +0400

    chore: implement api to get template preview
    take template data from database, prepare final html and
    send back to consumer

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,18 +1,24 @@
 import type { Template } from "../types.js";
 
-export function getTemplate(
-  objectId: string,
-  mock: boolean = true,
-): Promise<Template> {
-  return new Promise((res, rej) => {
-    if (mock) {
-      res({
-        id: "1",
-        html: '<div class="text-slate-500 bg-red-500 font-bold	">Hello World</div>',
-        styles: "",
-      });
-    } else {
-      rej(new Error("DB Integration not complete"));
-    }
-  });
+import { db } from "../db/index.js";
+import { mockTemplate } from "../utils/constants.js";
+
+export async function getTemplate(
+  templateId: string,
+  mock: boolean = false,
+): Promise<Template | null> {
+  if (mock) {
+    return mockTemplate;
+  }
+  const template = await db
+    .collection("templates")
+    .findOne({ template_id: templateId });
+  if (template) {
+    return {
+      templateId: template.templateId,
+      html: template.html,
+      styles: template.styles,
+    };
+  }
+  return null;
 }
