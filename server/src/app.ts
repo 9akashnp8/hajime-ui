0a52e7351dab6ad3443e816d4332f6a3cedc47f4
Akash NP <akashnp1998@gmail.com>
2023-12-09 13:10:29 +0400

    chore: move template generate to it's own controller
    separate generation logic into service function

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,19 +3,13 @@ import cors from "cors";
 import "dotenv/config";
 
 import { router } from "./routes.js";
-import { generateComponentCode } from "./utils/functions.js";
 
 const app = Express();
+app.use(Express.json());
 app.use(router);
 app.use(cors({ origin: "*" }));
 
 const PORT = 3000;
-
-app.post("/generate", async (req, res) => {
-  const generatedCode = await generateComponentCode("", true);
-  res.set("Content-Type", "text/html");
-  res.send(Buffer.from(generatedCode));
-});
 
 app.listen(PORT, () => {
   console.log(`Server Running on port: ${PORT}`); // eslint-disable-line no-console
