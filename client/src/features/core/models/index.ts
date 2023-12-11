7eac5a289cbec430dbeba6bcebce2463a72f4952
Akash NP <akashnp1998@gmail.com>
2023-12-11 23:29:14 +0400

    chore: minor changes

--- client/src/features/core/models/index.ts
+++
@@ -1,8 +0,0 @@
-import { Models } from '@rematch/core'
-import { generatedElement } from './generatedElement'
-
-export interface RootModel extends Models<RootModel> {
-    generatedElement: typeof generatedElement
-}
-
-export const models: RootModel = { generatedElement }
