f4bea40ec714464b68b41da33cbf75c48486b055
Akash NP <akashnp1998@gmail.com>
2024-01-10 19:48:38 +0400

    chore!: remove navbar, not serving any purpose

--- client/src/features/core/pages/root.tsx
+++ client/src/features/core/pages/root.tsx
@@ -1,11 +1,8 @@
 import { Outlet } from "react-router-dom"
 
-import NavBar from "../components/NavBar"
-
 export default function Root() {
     return (
         <div className="h-screen bg-slate-900 text-white p-5">
-            <NavBar />
             <Outlet />
         </div>
     )
